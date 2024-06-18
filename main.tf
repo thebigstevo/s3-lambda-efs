7810e6e288fc7ed74c28d47af35b7f790312abbf
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-18 18:43:38 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_vpc" "myvpc" {
+    id= "vpc-0e6c3c7402f44285d"
+  
+}
+resource "aws_s3_bucket" "receiving_bucket" {
+  bucket= "sle24-bucket"
+}
+
+resource "aws_efs_file_system" "efs_vol" {
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+}
+
+resource "aws_security_group" "efs_sg" {
+  name_prefix = "efs_sg"
+    vpc_id = data.aws_vpc.myvpc.id
+}
