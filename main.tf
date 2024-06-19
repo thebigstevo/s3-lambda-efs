80c5cad13d97d124069e03d0ed4505d5ad8a9343
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-19 23:21:54 +0100

    Added comments

--- main.tf
+++ main.tf
@@ -6,13 +6,15 @@ resource "aws_s3_bucket" "receiving_bucket" {
   bucket= "sle24-bucket"
 }
 
+
+#EFS file system
 resource "aws_efs_file_system" "efs_vol" {
   lifecycle_policy {
     transition_to_ia = "AFTER_30_DAYS"
   }
 }
 
-
+#EFS security group
 resource "aws_security_group" "efs_sg" {
   name_prefix = "efs_sg"
   vpc_id      = data.aws_vpc.myvpc.id
