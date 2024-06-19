4241a6162c6204f11e2b2280f5f70832206a42d7
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-19 01:55:39 +0100

    created data resource for vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-# data "aws_vpc" "myvpc" {
-#     id= "vpc-0e6c3c7402f44285d"
+data "aws_vpc" "myvpc" {
+    id= "vpc-0e6c3c7402f44285d"
   
-# }
+}
 # resource "aws_s3_bucket" "receiving_bucket" {
 #   bucket= "sle24-bucket"
 # }
