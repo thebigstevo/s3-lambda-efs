b7816aa4bc6898752da6b8e89b63c9dc504fe0ed
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-19 23:03:33 +0100

    removed duplicate security group

--- main.tf
+++ main.tf
@@ -12,7 +12,22 @@ resource "aws_efs_file_system" "efs_vol" {
   }
 }
 
+
 resource "aws_security_group" "efs_sg" {
   name_prefix = "efs_sg"
-    vpc_id = data.aws_vpc.myvpc.id
+  vpc_id      = data.aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.myvpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
