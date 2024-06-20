e8e993e910e620fa6b313ca17f66ea9ca2e91368
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-20 01:30:10 +0100

    updated typo

--- efs.tf
+++ efs.tf
@@ -6,11 +6,11 @@ resource "aws_efs_file_system" "efs_vol" {
 }
 
 resource "aws_efs_mount_target" "efs_mt" { 
- for_each = toset(data.aws_subnet.my_subnets.*.id)
+ for_each = toset(data.aws_subnets.my_subnets.*.id)
  
  file_system_id = aws_efs_file_system.efs_vol.id
  subnet_id      = each.value
- security_groups = [aws_security_group.sg_config.id]
+ security_groups = [aws_security_group.efs_sg.id]
 }
 
 resource "aws_efs_access_point" "efs_ap" {
