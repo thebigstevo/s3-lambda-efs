2b54a6c953519b37639e49bfc954b2ee2a7df370
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 20:39:47 +0100

    created vpc variables

--- variables.tf
+++ variables.tf
@@ -5,3 +5,23 @@ variable "vpc_id" {
 variable "vpc_name" {
 
 }
+
+variable "vpc_cidr" {
+  
+}
+
+variable "project_name" {
+  default = "s3tolambdatoefs"
+}
+
+variable "availability-zone-1" {
+  default = "eu-west-1a"
+}
+
+variable "availability-zone-2" {
+  default = "eu-west-1b"
+}
+
+variable "availability-zone-3" {
+  default = "eu-west-1c"
+}
