f112893edfb11e43b0bfd2135d7e551b8f71052a
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 21:29:36 +0100

    added s3 notification

--- s3.tf
+++ s3.tf
@@ -4,3 +4,11 @@ resource "aws_s3_bucket" "receiving_bucket" {
 }
 
 
+resource "aws_s3_bucket_notification" "s3_to_l_to_efs_notification" {
+  bucket = aws_s3_bucket.receiving_bucket.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3tolambdatoefs.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
