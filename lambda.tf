9c112d7b78f15cf85a258c236b7be5ae876adc4f
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 23:31:48 +0100

    added lambda function configuration

--- lambda.tf
+++ lambda.tf
@@ -1,26 +1,29 @@
-# resource "aws_lambda_function" "s3tolambdatoefs" {
-#   function_name = "s3_to_lambda_to_efs"
-#   handler       = "lambda_function.lambda_handler"
-#   role          = aws_iam_role.lambda_role.arn
-#   runtime = "python3.11"
-#   filename = "s3_to_lambda_to_efs.zip"
-#   source_code_hash = filebase64sha256("s3_to_lambda_to_efs.zip")
-#   vpc_config {
-#     security_group_ids = [aws_security_group.efs_sg.id]
-#     # Option 1: Using Indexing
-#     # subnet_ids= [data.aws_subnet.mysubnet[0].id, data.aws_subnet.mysubnet[1].id, ...]
+resource "aws_lambda_function" "s3tolambdatoefs" {
+  function_name = "s3_to_lambda_to_efs"
+  handler       = "lambda_function.lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+  runtime = "python3.11"
+  filename = "s3_to_lambda_to_efs.zip"
+  timeout = "20"
 
-#     # Option 2: Using for expression
-#     subnet_ids = [ for subnet in data.aws_subnet.mysubnet : subnet.id ]
-#   }
-#   environment {
-#     variables = {
-#       aws_efs_access_point= aws_efs_access_point.efs_ap.id
-#     }
-#   }
-#   file_system_config {
-#     arn = aws_efs_access_point.efs_ap.arn
-#     local_mount_path = "/mnt/efs"
+  source_code_hash = filebase64sha256("s3_to_lambda_to_efs.zip")
+  vpc_config {
+    security_group_ids = [aws_security_group.efs_sg.id]
+    # Option 1: Using Indexing
+    # subnet_ids= [data.aws_subnet.mysubnet[0].id, data.aws_subnet.mysubnet[1].id, ...]
+
+    # Option 2: Using for expression
+    subnet_ids = [ for subnet in data.aws_subnet.mysubnet : subnet.id ]
+  }
+  environment {
+    variables = {
+      aws_efs_access_point= aws_efs_access_point.efs_ap.id
+    }
+  }
+  file_system_config {
+    arn = aws_efs_access_point.efs_ap.arn
+    local_mount_path = "/mnt/efs"
     
-#   }
-# }
+  }
+}
+
