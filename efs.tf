539f1085c0b2b6c1d3b3c934d9e439109dc61d8e
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 22:56:57 +0100

    updated the efs mount targets properly

--- efs.tf
+++ efs.tf
@@ -6,11 +6,21 @@ resource "aws_efs_file_system" "efs_vol" {
 }
 
 
-resource "aws_efs_mount_target" "efs_mt" {
-  for_each = toset(data.aws_subnets.my_subnets.ids)
-
+resource "aws_efs_mount_target" "efs_mt-1" {
   file_system_id  = aws_efs_file_system.efs_vol.id
-  subnet_id       = each.value
+  subnet_id       = aws_subnet.public-subnet-1.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_efs_mount_target" "efs_mt-2" {
+  file_system_id  = aws_efs_file_system.efs_vol.id
+  subnet_id       = aws_subnet.public-subnet-2.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_efs_mount_target" "efs_mt-3" {
+  file_system_id  = aws_efs_file_system.efs_vol.id
+  subnet_id       = aws_subnet.public-subnet-3.id
   security_groups = [aws_security_group.efs_sg.id]
 }
 
