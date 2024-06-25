851a78b66b48a895fe55908a5b226525c9606c52
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 21:34:57 +0100

    added security group for efs

---
+++ security_group.tf
@@ -0,0 +1,24 @@
+#EFS security group
+resource "aws_security_group" "efs_sg" {
+  name_prefix = "efs_sg"
+  vpc_id      = data.aws_vpc.myvpc.id
+  description = "EFS security group"
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.myvpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "subnet_ids" {
+  value = data.aws_subnets.my_subnets.ids
+}
