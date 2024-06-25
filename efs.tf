e2b8aed78b358fe532f914429ae2a7706a7db7ef
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-25 20:39:16 +0100

    commented out other configurations

--- efs.tf
+++ efs.tf
@@ -1,35 +1,35 @@
-#EFS file system
-resource "aws_efs_file_system" "efs_vol" {
-  lifecycle_policy {
-    transition_to_ia = "AFTER_30_DAYS"
-  }
-}
+# #EFS file system
+# resource "aws_efs_file_system" "efs_vol" {
+#   lifecycle_policy {
+#     transition_to_ia = "AFTER_30_DAYS"
+#   }
+# }
 
 
-resource "aws_efs_mount_target" "efs_mt" {
-  for_each = toset(data.aws_subnets.my_subnets.ids)
+# resource "aws_efs_mount_target" "efs_mt" {
+#   for_each = toset(data.aws_subnets.my_subnets.ids)
 
-  file_system_id  = aws_efs_file_system.efs_vol.id
-  subnet_id       = each.value
-  security_groups = [aws_security_group.efs_sg.id]
-}
+#   file_system_id  = aws_efs_file_system.efs_vol.id
+#   subnet_id       = each.value
+#   security_groups = [aws_security_group.efs_sg.id]
+# }
 
-resource "aws_efs_access_point" "efs_ap" {
-  file_system_id = aws_efs_file_system.efs_vol.id
+# resource "aws_efs_access_point" "efs_ap" {
+#   file_system_id = aws_efs_file_system.efs_vol.id
 
 
-  posix_user {
-    gid = 1000
-    uid = 1000
-  }
+#   posix_user {
+#     gid = 1000
+#     uid = 1000
+#   }
 
-  root_directory {
-    path = "/"
+#   root_directory {
+#     path = "/"
 
-    creation_info {
-      owner_gid   = 1000
-      owner_uid   = 1000
-      permissions = "755"
-    }
-  }
-}
+#     creation_info {
+#       owner_gid   = 1000
+#       owner_uid   = 1000
+#       permissions = "755"
+#     }
+#   }
+# }
