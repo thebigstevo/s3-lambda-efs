e32dc61e2b6ff0ecda373f9d48be18cf08006c59
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-26 01:19:16 +0100

    changed the handler for troubleshooting

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "s3tolambdatoefs" {
   function_name = "s3_to_lambda_to_efs"
-  handler       = "s3tolambdatoefs.lambda_handler"
+  handler       = "s3_to_lambda_to_efs.lambda_handler"
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.8"
   filename      = "s3_to_lambda_to_efs.zip"
