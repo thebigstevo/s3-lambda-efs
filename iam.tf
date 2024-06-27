ddf76b9323c489a13dfd2dd027d25530ef41c143
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-06-27 21:43:26 +0100

    Added comments to iam

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# IAM Role for Lambda Function
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_s3_efs_role"
   assume_role_policy = jsonencode({
@@ -14,6 +15,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
+# IAM policy for Lambda
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda_s3_efs_policy"
   description = "Policy for Lambda to access S3 and EFS"
@@ -60,7 +62,7 @@ resource "aws_iam_policy" "lambda_policy" {
     }
   )
 }
-
+# Attach the IAM policy to the role
 resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
