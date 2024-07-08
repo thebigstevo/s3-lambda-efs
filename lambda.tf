c60f3d050e39aeb7b128cb40c629f6a9487056ce
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-07-08 21:14:34 +0100

    updated lambda configurations

--- lambda.tf
+++ lambda.tf
@@ -1,46 +1,48 @@
+# Lambda function
 resource "aws_lambda_function" "s3tolambdatoefs" {
   function_name = "s3_to_lambda_to_efs"
   handler       = "s3_to_lambda_to_efs.lambda_handler"
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.8"
   filename      = "s3_to_lambda_to_efs.zip"
-  timeout       = "300"
+  timeout       = "900" # Increased to the maximum timeout
   memory_size   = "1024"
 
-  source_code_hash = "s3_to_lambda_to_efs"
+  source_code_hash = filebase64sha256("s3_to_lambda_to_efs.zip")
   vpc_config {
-    security_group_ids = [aws_security_group.efs_sg.id]
-    subnet_ids         = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+    subnet_ids         = [
+      aws_subnet.public-subnet-1.id,
+      aws_subnet.public-subnet-2.id,
+      aws_subnet.public-subnet-3.id
+    ]
   }
+
   environment {
     variables = {
       aws_efs_access_point = aws_efs_access_point.efs_ap.id
     }
   }
+
   file_system_config {
     arn              = aws_efs_access_point.efs_ap.arn
     local_mount_path = "/mnt/efs"
-
   }
 
-  depends_on = [ aws_efs_access_point.efs_ap,
+  depends_on = [
+    aws_efs_access_point.efs_ap,
     aws_efs_mount_target.efs_mt-1,
     aws_efs_mount_target.efs_mt-2,
     aws_efs_mount_target.efs_mt-3,
-    aws_efs_access_point.efs_ap,
-    aws_efs_file_system.efs_vol, ]
+    aws_efs_file_system.efs_vol
+  ]
 }
 
-# data "archive_file" "lambda_zip" {Í
-#  type = "zip"
-#  source_dir = "module/lambda"
-#  output_path = "module/lambda/s3_to_lambda_to_efs.zip"
-# }
+# Lambda permissions for S3
 resource "aws_lambda_permission" "with_s3" {
   statement_id  = "s3invokelambda"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3tolambdatoefs.arn
+  function_name = aws_lambda_function.s3tolambdatoefs.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.receiving_bucket.arn
 }
-
