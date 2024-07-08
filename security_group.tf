a0ed581ade268771f63d1e445c0738b9a7dce683
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-07-08 21:21:59 +0100

    updated rules

--- security_group.tf
+++ security_group.tf
@@ -1,4 +1,4 @@
-#EFS security group
+# EFS security group
 resource "aws_security_group" "efs_sg" {
   name_prefix = "efs_sg"
   vpc_id      = aws_vpc.s3toefs-vpc.id
@@ -8,7 +8,7 @@ resource "aws_security_group" "efs_sg" {
     from_port   = 2049
     to_port     = 2049
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.s3toefs-vpc.cidr_block]
+    security_groups = [aws_security_group.lambda_sg.id]
   }
 
   egress {
@@ -19,23 +19,18 @@ resource "aws_security_group" "efs_sg" {
   }
 }
 
-
-resource "aws_security_group" "ec2-sg" {
-  vpc_id      = aws_vpc.s3toefs-vpc.id
-  name        = "ec2-sg"
-  description = "EC2 security group"
-  ingress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
+# Lambda security group
 resource "aws_security_group" "lambda_sg" {
   name_prefix = "lambda_sg"
   vpc_id      = aws_vpc.s3toefs-vpc.id
   description = "Lambda security group"
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.efs_sg.id]
+  }
 
   egress {
     from_port   = 0
