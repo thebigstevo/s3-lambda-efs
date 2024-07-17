ebfd04613a8a0707df1e4c0afb4df371aca97d7a
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-07-17 20:13:09 +0100

    disabled lambda function

--- main.tf
+++ main.tf
@@ -33,24 +33,24 @@ module "s3" {
   source     = "./modules/s3"
   vpc_id     = module.vpc.vpc_id
   route_table_id= module.vpc.public_route_table_id
-  lambda_arn = module.lambda.lambda_arn
-  function_name = module.lambda.function_name
+  # lambda_arn = module.lambda.lambda_arn
+  # function_name = module.lambda.function_name
   depends_on = [module.vpc]
 }
 
 
 
-module "lambda" {
-  source     = "./modules/lambda"
-  public_subnet_1_id = module.vpc.public_subnet_1_id
-  public_subnet_2_id = module.vpc.public_subnet_2_id
-  public_subnet_3_id = module.vpc.public_subnet_3_id
-  efs_access_point_arn = module.efs.efs_access_point_arn
-  lambda_security_group_ids = module.security_groups.lambda_security_group_ids
-  s3_bucket_arn = module.s3.s3_bucket_arn
-  lambda_role_arn = module.iam.lambda_role_arn
+# module "lambda" {
+#   source     = "./modules/lambda"
+#   public_subnet_1_id = module.vpc.public_subnet_1_id
+#   public_subnet_2_id = module.vpc.public_subnet_2_id
+#   public_subnet_3_id = module.vpc.public_subnet_3_id
+#   efs_access_point_arn = module.efs.efs_access_point_arn
+#   lambda_security_group_ids = module.security_groups.lambda_security_group_ids
+#   s3_bucket_arn = module.s3.s3_bucket_arn
+#   lambda_role_arn = module.iam.lambda_role_arn
 
-  depends_on = [module.efs, module.iam, module.s3]
-}
+#   depends_on = [module.efs, module.iam, module.s3]
+# }
 
 
