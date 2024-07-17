39925f688cb1e5642b88eceed59b552127b9a72d
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-07-17 19:32:40 +0100

    enabled only vpc and sg

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 module "vpc" {
   source       = "./modules/network"
   project_name = var.project_name
-  vpc_id = module.vpc.vpc_id
   vpc_cidr = var.vpc_cidr
 
 }
@@ -13,41 +12,43 @@ module "security_groups"{
   depends_on = [module.vpc]
 }
 
-module "iam" {
-  source = "./modules/iam"
+# module "iam" {
+#   source = "./modules/iam"
 
-}
+# }
 
-module "lambda" {
-  source     = "./modules/lambda"
-  public_subnet_1_id = module.vpc.public_subnet_1_id
-  public_subnet_2_id = module.vpc.public_subnet_2_id
-  public_subnet_3_id = module.vpc.public_subnet_3_id
-  efs_access_point_arn = module.efs.efs_access_point_arn
-  lambda_security_group_ids = module.security_groups.lambda_security_group_ids
-  s3_bucket_arn = module.s3.s3_bucket_arn
-  lambda_role_arn = module.iam.lambda_role_arn
-
-  depends_on = [module.efs, module.iam, module.s3]
-}
-
-module "efs" {
-  source     = "./modules/efs"
-  public_subnet_1_id = module.vpc.public_subnet_1_id
-  public_subnet_2_id = module.vpc.public_subnet_2_id
-  public_subnet_3_id = module.vpc.public_subnet_3_id
-  efs_sg_id = module.security_groups.efs_security_group_ids
+# module "efs" {
+#   source     = "./modules/efs"
+#   public_subnet_1_id = module.vpc.public_subnet_1_id
+#   public_subnet_2_id = module.vpc.public_subnet_2_id
+#   public_subnet_3_id = module.vpc.public_subnet_3_id
+#   efs_sg_id = module.security_groups.efs_security_group_ids
   
   
-  depends_on = [module.vpc]
-}
+#   depends_on = [module.vpc]
+# }
 
-module "s3" {
-  source     = "./modules/s3"
-  vpc_id     = module.vpc.vpc_id
-  route_table_id= module.vpc.public_route_table_id
-  function_name = module.lambda.function_name
-  lambda_arn = module.lambda.lambda_arn
-  depends_on = [module.vpc]
-}
+# module "s3" {
+#   source     = "./modules/s3"
+#   vpc_id     = module.vpc.vpc_id
+#   route_table_id= module.vpc.public_route_table_id
+#   lambda_arn = module.lambda.lambda_arn
+#   depends_on = [module.vpc]
+# }
+
+
+
+# module "lambda" {
+#   source     = "./modules/lambda"
+#   public_subnet_1_id = module.vpc.public_subnet_1_id
+#   public_subnet_2_id = module.vpc.public_subnet_2_id
+#   public_subnet_3_id = module.vpc.public_subnet_3_id
+#   efs_access_point_arn = module.efs.efs_access_point_arn
+#   lambda_security_group_ids = module.security_groups.lambda_security_group_ids
+#   s3_bucket_arn = module.s3.s3_bucket_arn
+#   lambda_role_arn = module.iam.lambda_role_arn
+
+#   depends_on = [module.efs, module.iam, module.s3]
+# }
+
 
--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -29,3 +29,13 @@ resource "aws_lambda_function" "s3tolambdatoefs" {
     local_mount_path = "/mnt/efs"
   }
 }
+
+
+# Lambda permissions for S3
+resource "aws_lambda_permission" "with_s3" {
+  statement_id  = "s3invokelambda"
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = var.s3_bucket_arn
+}
