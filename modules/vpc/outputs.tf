dac75d20a0ed56fa3cda76046a20fe3e27ac0e68
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-07-19 01:17:30 +0100

    update code

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,9 +3,18 @@ output "vpc_id"{
     value = aws_vpc.s3toefs-vpc.id
 }
 
-output "public_subnet_ids" {
-  value = aws_subnet.public_subnet[*].id
+output "public_subnet_1_id" {
+  value = aws_subnet.public_subnet[0].id
 }
+
+output "public_subnet_2_id" {
+  value = aws_subnet.public_subnet[1].id
+}
+
+output "public_subnet_3_id" {
+  value = aws_subnet.public_subnet[2].id
+}
+
 output "public_route_table_id" {
   value = aws_route_table.public_route_table.id
 }
