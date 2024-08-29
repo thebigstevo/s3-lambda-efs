c67e96607d3803a02756b9a40bf9dee2c5b55216
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-08-29 13:41:12 +0100

    removed extra script

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -21,8 +21,6 @@ resource "aws_instance" "test_server" {
     sudo yum install -y amazon-efs-utils
     sudo mount -t efs -o tls,accesspoint="${var.efs_access_point_id}" "${var.efs_file_system_id}":/ efs
     
-    # Create the subfolders
-    sudo mkdir -p /efs/sst /efs/sdc
   EOF
 
     }
